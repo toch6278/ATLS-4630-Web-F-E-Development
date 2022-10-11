37e9a6d40d66d926edd39a8867631590fc245a7d
toch6278 <toch6278@colorado.edu>
2022-10-11 12:10:11 -0600

    fix matching cards

--- Projects/Project1/Levels/index.js
+++ Projects/Project1/Levels/index.js
@@ -14,13 +14,14 @@ function flipCard(e)
     //gets user clicked card
     let clickedCard = e.target; 
     //identifies what the element is in the html script
-    console.log(e.target); 
+    // console.log(e.target); 
     
-    
+    //if the card pressed wasnt the first card chosen
     if(clickedCard !== card1)
     {
         // made the cards part of the flip class when clicked
         clickedCard.classList.add('flip'); 
+        
         if(!card1)
         {
             //return chosen card1
@@ -29,8 +30,12 @@ function flipCard(e)
         card2 = clickedCard; 
         console.log(card1,card2);
 
-        let card1Img = card1.querySelector("img").src; 
-        let card2Img = card2.querySelector("img").src; 
+        let card1Img = card1.querySelector("#mochi").src; 
+        let card2Img = card2.querySelector("#mochi").src; 
+        // let card1Img = document.getElementsByClassName("view back-view").src; 
+        // card2Img = document.querySelector("view back-view"); 
+        // console.log(card1Img); 
+        // console.log(card2Img); 
         matchCards(card1Img, card2Img);
     }
     
@@ -38,9 +43,29 @@ function flipCard(e)
 
 function matchCards(img1, img2) 
 {
+    console.log(img1); 
+    console.log(img2); 
     if (img1 === img2)
     {
+        // card1.style.display = "none"; 
+        // card2.style.display = "none";
+        
+        card1.querySelector("#mochi").disabled = true; 
+        card2.querySelector("#mochi").disabled = true;
         return console.log("card matched"); 
     }
     console.log("no match");
+
+    setTimeout(() => {
+        //adding shake to class to both card after 400ms        
+        card1.classList.add("shake");
+        card2.classList.add("shake");
+    }, 400);
+    setTimeout(() => {
+        //adding shake to class to both card after 400ms        
+        card1.classList.remove("shake", "flip");
+        card2.classList.remove("shake", "flip")
+        card1 = ""; 
+        card2 = "";
+    }, 1200);
 }
