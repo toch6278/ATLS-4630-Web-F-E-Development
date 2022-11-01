7006d6735dcb0db014bdde2a42b32c78c7aa325d
toch6278 <toch6278@colorado.edu>
2022-11-01 10:45:59 -0600

    working the app.js in react

--- ClassCode/Week11/11-01-react-example/src/App.js
+++ ClassCode/Week11/11-01-react-example/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Title from './Title.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+
+    
+    <div className = "App"> 
+      {/* <Title /> */}
+      {/* normal properties  */}
+      <Title name = "Torsh"/>
+      <Title name = "Not Peter"/>
     </div>
   );
 }
--- ClassCode/Week11/11-01-react-example/src/Title.js
+++ ClassCode/Week11/11-01-react-example/src/Title.js
@@ -1,12 +1,55 @@
 import React from 'react';
+
+//multiple file component
+// import 'Title.css';
+
 export default class Title extends React.Component{
-    constructor(name)
+    // constructor(name)
+    // {
+    //     //call super constructor of the constructor in order to extend
+    //     //associate with react very heavily 
+    //     super();
+    //     // this.name = name;
+    // }
+    // render() {
+    //     return (
+    //         // <h1> {this.name} </h1>
+    //         <h1> Hello world! </h1>
+    //     );
+    // }
+
+    //never actually call constructor
+    constructor(props)
     {
-        this.name = name;
+        //call super constructor of the constructor in order to extend
+        //associate with react very heavily 
+        super();
+        //identify properties (props)
+        this.name = props.name;
+
+        //property validations
+        // this.name = props.name =/= undefined ? props.name : 'default name';
     }
+    // render() {
+    //     return (
+    //         // <h1> {this.name} </h1>
+    //         <h1> Hello {this.name}! </h1>
+    //     );
+    // }
+
+    //react way of adding styling 
     render() {
+        const styles = {
+            fontSize: '2em',
+            color: 'lightblue'
+        };
+        //if you wanted to style your divs a certain way 
+        // const divStyles
         return (
-            <h1> {name} </h1>
-        )
+            // <h1> {this.name} </h1>
+            <h1 style = {styles}> Hello {this.name}! </h1>
+            // <h1 style = {h1Styles}> Hello {this.name}! </h1>
+            // <h1 className = "my-heading" style = {h1Styles}> Hello {this.name}! </h1>
+        );
     }
 } 
