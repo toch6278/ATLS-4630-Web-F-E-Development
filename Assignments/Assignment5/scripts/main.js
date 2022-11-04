393094b68c06a6858b0c80d4a469e47c71ce5fe9
toch6278 <toch6278@colorado.edu>
2022-11-03 20:43:20 -0600

    11-03 class code all

--- Assignments/Assignment5/scripts/main.js
+++ Assignments/Assignment5/scripts/main.js
@@ -1,17 +1,72 @@
-import * as  RiTa from 'rita.js';
+import * as THREE from 'three';
 
-console.log('hello');
+//make sure that the THREE object exists
+console.log(THREE);
 
-rhymeButton = document.querySelector("#find");
-rhymeButton.addEventListener('click', () => {
-    console.log("reloading");
-    window.location.reload(true); 
+//how to create a THREE scene
+const scene = new THREE.Scene(); 
+
+//creating a THREE camera (can only use fully complete cameras - ortho or linear)
+const camera = new THREE.PerspectiveCamera(
+    75, 
+    window.innerWidth / window.innerHeight, 
+    0.1, 
+    1000,
+);
+
+//be able to render a picture - create it's own canvas to put into document 
+const renderer = new THREE.WebGL1Renderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+//create canvas in the body
+document.body.appendChild(renderer.domElement);
+
+//THREE light: add directional light and ambient light 
+const ambient = new THREE.AmbientLight(0xffffff, 1);
+//if don't add ight to the scene, it won't show anything
+scene.add(ambient); 
+const directional = new THREE.DirectionalLight(0xffffff, 0.9);
+//will point towards the origin so should change the direction
+directional.position.x = 7; 
+directional.position.y = 14; 
+scene.add(directional);
+
+//create text 
+const text = new THREE.TextGeometry('Torsh');
+
+//create a cube 
+const geometry = new THREE.BoxGeometry(2,1,1);
+const material = new THREE.MeshPhysicalMaterial({
+    color: 0x00ff00,
+    roughness: 0.5,
+    metalness: 0.7,
 });
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+camera.position.z = 5; 
 
-function findRhymes() {
-    let rhymes = RiTa.rhymes(field.value());
-    if (rhymes.length > 0) {
-      text(rhymes.join(" "),
-        10, 10, width-10, height-10);
-    }
-  }
+const loadModel = new THREE.ObjectLoader();
+loadModel.load("Ch19_nonPBR.fbx");
+
+// function ( obj ) {
+//   scene.add(obj);
+// }
+
+// function( err ) {
+//   console.error(`Can't load model`)
+// }
+// const model = loader.parse(a_)
+
+//create a render loop, called every 60th of a second
+function loop()
+{
+    //modify the scene 
+    mesh.rotation.x += 0.01;
+    // mesh.rotateX(0.01); 
+    mesh.rotateZ(0.01); 
+    //render the scene, should be called last so can render all the changes
+    renderer.render(scene,camera);
+    //make it a loop
+    requestAnimationFrame(loop); 
+}
+requestAnimationFrame(loop); 
